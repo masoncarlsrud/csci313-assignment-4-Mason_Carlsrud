b23dd09f57cade8758f8122f8564e62c585880cb
Masonac <masonac00@gmail.com>
2024-04-04 00:05:59 -0500

    still attempting to make populate() work and still failing

--- manage.py
+++ manage.py
@@ -9,13 +9,12 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locallibrary.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+        execute_from_command_line(sys.argv)
+    except RuntimeError as exc:
+        if str(exc) == "populate() isn't reentrant":
+            print("Error: populate() function is not reentrant.")
+        else:
+            raise exc
 
 if __name__ == '__main__':
     main()
